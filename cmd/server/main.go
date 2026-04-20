4ed88a6e03786955b491b895b07c7e2e066b933e
Travis <mynameismyname1992@gmail.com>
2026-04-20 13:27:05 -0500

    CH2 L4: Make connection to RabbitMQ server with amqp. Wait for exit signal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const connectionStr = "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Fatal("Could not make connection to RabbitMQ:", err)
+	}
+	defer connection.Close()
+	fmt.Println("Peril game server successfully connected to RabbitMQ!")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("\nRabbitMQ connection closed...")
 }
