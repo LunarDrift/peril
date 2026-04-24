5360734f4091873e0dc3dafa39d4d33f27b22996
= <mynameismyname1992@gmail.com>
2026-04-24 07:47:14 -0500

    Update server so it can interactively pause and resume game

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -26,16 +27,34 @@ func main() {
 		log.Fatal("Could not create AMQP Channel:", err)
 	}
 
-	err = pubsub.PublishJSON(
-		publishChannel,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Printf("could not Publish Playing State: %v", err)
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "pause":
+			fmt.Println("Publishing paused game state")
+			err = pubsub.PublishJSON(publishChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+
+		case "resume":
+			fmt.Println("Publishing resumes game state")
+			err = pubsub.PublishJSON(publishChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+
+		case "quit":
+			log.Println("goodbye")
+			return
+
+		default:
+			fmt.Println("unknown command")
+		}
 	}
-	fmt.Println("Pause message sent!")
 }
