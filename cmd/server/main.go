34faea2e55aafd15ac14eb54438b97cde06de672
= <mynameismyname1992@gmail.com>
2026-04-25 17:04:21 -0500

    Update server to declare and bind new queue to peril_topic exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,6 +27,18 @@ func main() {
 		log.Fatal("Could not create AMQP Channel:", err)
 	}
 
+	_, queue, err := pubsub.DeclareAndBind(
+		connection,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.SimpleQueueDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to pause: %v", err)
+	}
+	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
+
 	gamelogic.PrintServerHelp()
 
 	for {
